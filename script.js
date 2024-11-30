64648810381844d9e2d5124cb37c430376dc16cb
Chandan Kumar <kumarchandan99990@gmail.com>
2024-11-30 20:10:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,8 @@ setInterval(() => {
   testimonials[currentIndex].style.display = 'block';
 }, 3000);
 // JavaScript to toggle the navigation menu
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
-
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
+document.getElementById("hamburger").addEventListener("click", function () {
+  const navMenu = document.getElementById("nav-menu").querySelector("ul");
+  navMenu.classList.toggle("active");
 });
 
